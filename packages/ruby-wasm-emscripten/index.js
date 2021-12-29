e86b714645bcdfae726b45e4c533d2ca785e7588
Yuta Saito <kateinoigakukun@gmail.com>
2021-12-29 22:12:51 +0900

    Don't preset defaultModule

--- packages/ruby-wasm-emscripten/index.js
+++ packages/ruby-wasm-emscripten/index.js
@@ -1,10 +1,7 @@
 import { createRubyModule } from "./dist/ruby"
 import { loadRubyStdlib } from "./dist/ruby_stdlib"
 
-export const loadRuby = async (baseURL) => {
-  const defaultModule = {
-    locateFile: (path) => baseURL + path
-  };
+export const loadRuby = async (defaultModule) => {
   const RubyModule = await createRubyModule(defaultModule);
   globalThis.__ruby_module = RubyModule;
   loadRubyStdlib();
