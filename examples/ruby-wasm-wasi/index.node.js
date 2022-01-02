d914b27ccc9728d52dcc97aa94a8324484d4de04
Yuta Saito <kateinoigakukun@gmail.com>
2022-01-02 21:45:46 +0900

    Restore random example code

--- examples/ruby-wasm-wasi/index.node.js
+++ examples/ruby-wasm-wasi/index.node.js
@@ -17,30 +17,35 @@ const main = async () => {
   await vm.init(instance);
   // Start WASI application
   wasi.initialize(instance);
-  vm.guest.rubyInit();
-  // const args = ["ruby.wasm\0", "--version\0"];
+  vm.guest.rubyShowVersion();
   const args = ["ruby.wasm\0", "-e\0", "_=0\0"];
-  // vm.guest.rubySysinit(args);
+  vm.guest.rubySysinit(args);
+  vm.guest.rubyInit();
   vm.guest.rubyOptions(args)
-  // /vm.guest.rubyShowVersion();
-  // /vm.guest.rubyInit();
-  // /const a = vm.eval(`
-  // /class A
-  // /  def foo(arg)
-  // /    puts "yay: #{arg}"
-  // /  end
-  // /end
-  // /A.new
-  // /`)
-  // /// console.log(`${a}`)
-  // /a.bar()
-  // /vm.eval("puts 'Hey!'");
-  // /vm.eval("puts 'Hey!'");
-  // /vm.eval("puts 'Hey!'");
-  // /// vm.eval("puts $a");
-  // /console.log(a.toString())
-  // console.log(a.toString())
-  // vm.eval("raise 'panic!'")
+  const a = vm.eval(`
+  class A
+    def foo(arg)
+      puts "yay: #{arg}"
+    end
+  end
+  A.new
+  `)
+  console.log(`${a}`)
+  try {
+    a.call("bar")
+  } catch (error) {
+    console.log("caught", error)
+  }
+  vm.eval("puts 'Hey!'");
+  vm.eval("puts 'Hey!'");
+  vm.eval("puts 'Hey!'");
+  vm.eval("puts $a");
+  try {
+    vm.eval("raise 'panic!'")
+  } catch (error) {
+    console.log("caught", error)
+  }
+  console.log(a.toString())
 };
 
 main()
