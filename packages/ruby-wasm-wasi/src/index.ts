1a812e2ea699cbe67702e583ce96b5eb854aa388
Yuta Saito <kateinoigakukun@gmail.com>
2022-01-03 01:26:15 +0900

    Improve API

--- packages/ruby-wasm-wasi/src/index.ts
+++ packages/ruby-wasm-wasi/src/index.ts
@@ -1,38 +1,117 @@
 import * as RbAbi from "./bindgen/rb-js-abi-guest";
 import { addRbJsAbiHostToImports } from "./bindgen/rb-js-abi-host";
 
-enum ruby_value_type {
-  None = 0x00,
+/**
+ * Error class thrown by Ruby execution
+ */
+export class RbError extends Error {
+  constructor(message: string) {
+    super(message)
+  }
+}
 
-  Object = 0x01,
-  Class = 0x02,
-  Module = 0x03,
-  Float = 0x04,
-  RString = 0x05,
-  Regexp = 0x06,
-  Array = 0x07,
-  Hash = 0x08,
-  Struct = 0x09,
-  Bignum = 0x0a,
-  File = 0x0b,
-  Data = 0x0c,
-  Match = 0x0d,
-  Complex = 0x0e,
-  Rational = 0x0f,
+/**
+ * A Ruby VM instance
+ *
+ * ## Example
+ * 
+ * ```javascript
+ * const wasi = new WASI();
+ * const vm = new RubyVM();
+ * const imports = {
+ *   wasi_snapshot_preview1: wasi.wasiImport,
+ * };
+ *
+ * vm.addToImports(imports);
+ *
+ * const instance = await WebAssembly.instantiate(rubyModule, imports);
+ * await vm.init(instance);
+ * wasi.initialize(instance);
+ * ```
+ */
+export class RubyVM {
+  guest: RbAbi.RbJsAbiGuest;
+  private instance: WebAssembly.Instance | null = null;
 
-  Nil = 0x11,
-  True = 0x12,
-  False = 0x13,
-  Symbol = 0x14,
-  Fixnum = 0x15,
-  Undef = 0x16,
+  constructor() {
+    this.guest = new RbAbi.RbJsAbiGuest();
+  }
 
-  IMemo = 0x1a,
-  Node = 0x1b,
-  IClass = 0x1c,
-  Zombie = 0x1d,
+  /**
+   * Initialize a given instance to interact JavaScript and Ruby's
+   * WebAssembly instance. This method must be called before calling
+   * Ruby API.
+   *
+   * The `instance` must be instantiated from a Ruby built with JS
+   * extension, and built with Reactor ABI instead of command line.
+   */
+  async init(instance: WebAssembly.Instance) {
+    this.instance = instance;
+    await this.guest.instantiate(instance);
+  }
 
-  Mask = 0x1f,
+  /**
+   * Add intrinsic import entries, which is necessary to interact JavaScript
+   * and Ruby's WebAssembly instance.
+   */
+  addToImports(imports: WebAssembly.Imports) {
+    this.guest.addToImports(imports);
+    addRbJsAbiHostToImports(
+      imports,
+      {
+        evalJs: (code) => {
+          new Function(code)();
+        },
+      },
+      (name) => {
+        return this.instance.exports[name];
+      }
+    );
+  }
+
+  /**
+   * Print the Ruby version to stdout
+   */
+  printVersion() {
+    this.guest.rubyShowVersion();
+  }
+
+  /**
+   * Runs a string of Ruby code from JavaScript
+   * Returns the result of the last expression
+   */
+  eval(code: string): RbValue {
+    return evalRbCode(this.guest, code);
+  }
+}
+
+
+/**
+ * A RbValue is an object that represents a value in Ruby
+ */
+export class RbValue {
+  constructor(public inner: RbAbi.RbValue, private guest: RbAbi.RbJsAbiGuest) {}
+
+  /**
+   * Call a given method with given arguments
+   */
+  call(callee: string, ...args: RbValue[]): RbValue {
+    const innerArgs = args.map(arg => arg.inner);
+    return new RbValue(callRbMethod(this.guest, this.inner, callee, innerArgs), this.guest)
+  }
+
+  [Symbol.toPrimitive](hint: string) {
+    if (hint === "string" || hint === "default") {
+      return this.toString();
+    } else if (hint === "number") {
+      return null;
+    }
+    return null;
+  }
+  toString(): string {
+    const rbString = callRbMethod(this.guest, this.inner, "to_s", []);
+    return this.guest.rstringPtr(rbString);
+  }
 }
 
 enum ruby_tag_type {
@@ -48,11 +127,6 @@ enum ruby_tag_type {
   Mask	  = 0xf
 };
 
-class RbError extends Error {
-  constructor(message: string) {
-    super(message)
-  }
-}
 
 const formatException = (klass: string, message: string, backtrace: [string, string]) => {
   return `${backtrace[0]}: ${message} (${klass})\n${backtrace[1]}`
@@ -87,7 +161,6 @@ const checkStatusTag = (rawTag: number, guest: RbAbi.RbJsAbiGuest) => {
   }
 }
 
-const projectRbRawValue = (v: RbAbi.RbValue) => (v as any)._wasm_val as number;
 const callRbMethod = (guest: RbAbi.RbJsAbiGuest, recv: RbAbi.RbValue, callee: string, args: RbAbi.RbValue[]) => {
   const mid = guest.rbIntern(callee + "\0");
   const [value, status] = guest.rbFuncallvProtect(recv, mid, args);
@@ -98,69 +171,4 @@ const evalRbCode = (guest: RbAbi.RbJsAbiGuest, code: string) => {
   const [value, status] = guest.rbEvalStringProtect(code + "\0");
   checkStatusTag(status, guest);
   return new RbValue(value, guest);
-}
-
-export class RbValue {
-  constructor(public inner: RbAbi.RbValue, private guest: RbAbi.RbJsAbiGuest) {}
-
-  call(prop: string, ...args: RbValue[]): RbValue {
-    const innerArgs = args.map(arg => arg.inner);
-    return new RbValue(callRbMethod(this.guest, this.inner, prop, innerArgs), this.guest)
-  }
-
-  rawValue(): number {
-    return (this.inner as any)._wasm_val;
-  }
-  [Symbol.toPrimitive](hint: string) {
-    if (hint === "string" || hint === "default") {
-      return this.toString();
-    } else if (hint === "number") {
-      return null;
-    }
-    return null;
-  }
-  toString(): string {
-    const rbString = callRbMethod(this.guest, this.inner, "to_s", []);
-    return this.guest.rstringPtr(rbString);
-  }
-}
-
-export class RubyVM {
-  guest: RbAbi.RbJsAbiGuest;
-  private instance: WebAssembly.Instance | null = null;
-  constructor() {
-    this.guest = new RbAbi.RbJsAbiGuest();
-  }
-
-  async init(instance: WebAssembly.Instance) {
-    this.instance = instance;
-    await this.guest.instantiate(instance);
-  }
-
-  addToImports(imports: WebAssembly.Imports) {
-    this.guest.addToImports(imports);
-    addRbJsAbiHostToImports(
-      imports,
-      {
-        evalJs: (code) => {
-          new Function(code)();
-        },
-      },
-      (name) => {
-        return this.instance.exports[name];
-      }
-    );
-  }
-
-  printVersion() {
-    this.guest.rubyShowVersion();
-  }
-
-  /**
-   * Runs a string of Ruby code from JavaScript
-   * Returns the result of the last expression
-   */
-  eval(code: string): RbValue {
-    return evalRbCode(this.guest, code);
-  }
 }
