657c1e595c7f54044a594d325e6b063cac95cade
Yuta Saito <kateinoigakukun@gmail.com>
2022-01-09 01:53:06 +0900

    format c sources

--- ext/js/js-core.c
+++ ext/js/js-core.c
@@ -5,19 +5,20 @@
 #include "bindgen/rb-js-abi-guest.h"
 #include "bindgen/rb-js-abi-host.h"
 
-__attribute__((import_module("asyncify"), import_name("start_unwind")))
-void asyncify_start_unwind(void *buf);
-__attribute__((import_module("asyncify"), import_name("stop_unwind")))
-void asyncify_stop_unwind(void);
-__attribute__((import_module("asyncify"), import_name("start_rewind")))
-void asyncify_start_rewind(void *buf);
-__attribute__((import_module("asyncify"), import_name("stop_rewind")))
-void asyncify_stop_rewind(void);
+__attribute__((import_module("asyncify"), import_name("start_unwind"))) void
+asyncify_start_unwind(void *buf);
+__attribute__((import_module("asyncify"), import_name("stop_unwind"))) void
+asyncify_stop_unwind(void);
+__attribute__((import_module("asyncify"), import_name("start_rewind"))) void
+asyncify_start_rewind(void *buf);
+__attribute__((import_module("asyncify"), import_name("stop_rewind"))) void
+asyncify_stop_rewind(void);
 
 void *rb_wasm_handle_jmp_unwind(void);
 void *rb_wasm_handle_scan_unwind(void);
 void *rb_wasm_handle_fiber_unwind(void (**new_fiber_entry)(void *, void *),
-                                  void **arg0, void **arg1, bool *is_new_fiber_started);
+                                  void **arg0, void **arg1,
+                                  bool *is_new_fiber_started);
 
 #define RB_WASM_LIB_RT(MAIN_ENTRY)                                             \
   {                                                                            \
@@ -58,32 +59,29 @@ void *rb_wasm_handle_fiber_unwind(void (**new_fiber_entry)(void *, void *),
     }                                                                          \
   }
 
-#define c_strings_from_abi(list, new_args) \
-  { \
-    new_args = alloca(sizeof(char *) * ((list)->len + 1)); \
-    for (size_t i = 0; i < (list)->len; i++) { \
-      new_args[i] = (list)->ptr[i].ptr; \
-    } \
+#define c_strings_from_abi(list, new_args)                                     \
+  {                                                                            \
+    new_args = alloca(sizeof(char *) * ((list)->len + 1));                     \
+    for (size_t i = 0; i < (list)->len; i++) {                                 \
+      new_args[i] = (list)->ptr[i].ptr;                                        \
+    }                                                                          \
   }
 
-
 // MARK: - Exported functions
-// NOTE: Assume that callers always pass null terminated string by rb_js_abi_guest_string_t
+// NOTE: Assume that callers always pass null terminated string by
+// rb_js_abi_guest_string_t
 
-void rb_js_abi_guest_ruby_show_version(void) {
-  ruby_show_version();
-}
+void rb_js_abi_guest_ruby_show_version(void) { ruby_show_version(); }
 
-void rb_js_abi_guest_ruby_init(void) {
-  RB_WASM_LIB_RT(ruby_init())
-}
-#include <stdio.h>
+void rb_js_abi_guest_ruby_init(void) { RB_WASM_LIB_RT(ruby_init()) }
+
 void rb_js_abi_guest_ruby_sysinit(rb_js_abi_guest_list_string_t *args) {
   char **c_args;
   int argc = args->len;
   c_strings_from_abi(args, c_args);
   RB_WASM_LIB_RT(ruby_sysinit(&argc, &c_args))
 }
+
 rb_js_abi_guest_rb_iseq_t
 rb_js_abi_guest_ruby_options(rb_js_abi_guest_list_string_t *args) {
   void *result;
@@ -92,6 +90,7 @@ rb_js_abi_guest_ruby_options(rb_js_abi_guest_list_string_t *args) {
   RB_WASM_LIB_RT(result = ruby_options(args->len, c_args))
   return rb_js_abi_guest_rb_iseq_new(result);
 }
+
 rb_js_abi_guest_rb_errno_t
 rb_js_abi_guest_ruby_run_node(rb_js_abi_guest_rb_iseq_t node) {
   int result;
@@ -99,14 +98,18 @@ rb_js_abi_guest_ruby_run_node(rb_js_abi_guest_rb_iseq_t node) {
   RB_WASM_LIB_RT(ruby_run_node(iseq))
   return result;
 }
+
 void rb_js_abi_guest_ruby_script(rb_js_abi_guest_string_t *name) {
   RB_WASM_LIB_RT(ruby_script(name->ptr))
 }
+
 void rb_js_abi_guest_ruby_init_loadpath(void) {
   RB_WASM_LIB_RT(ruby_init_loadpath())
 }
 
-void rb_js_abi_guest_rb_eval_string_protect(rb_js_abi_guest_string_t *str, rb_js_abi_guest_rb_value_t *result, int32_t *state) {
+void rb_js_abi_guest_rb_eval_string_protect(rb_js_abi_guest_string_t *str,
+                                            rb_js_abi_guest_rb_value_t *result,
+                                            int32_t *state) {
   VALUE retval;
   RB_WASM_LIB_RT(retval = rb_eval_string_protect(str->ptr, state));
   // TODO(katei): protect the value from GC
@@ -129,17 +132,23 @@ VALUE rb_funcallv_thunk(VALUE arg) {
   return rb_funcallv(ctx->recv, ctx->mid, ctx->args->len, c_argv);
 }
 
-void rb_js_abi_guest_rb_funcallv_protect(rb_js_abi_guest_rb_value_t recv, rb_js_abi_guest_rb_id_t mid, rb_js_abi_guest_list_rb_value_t *args, rb_js_abi_guest_rb_value_t *ret0, int32_t *ret1) {
+void rb_js_abi_guest_rb_funcallv_protect(rb_js_abi_guest_rb_value_t recv,
+                                         rb_js_abi_guest_rb_id_t mid,
+                                         rb_js_abi_guest_list_rb_value_t *args,
+                                         rb_js_abi_guest_rb_value_t *ret0,
+                                         int32_t *ret1) {
   VALUE retval;
   VALUE abi_recv = (VALUE)rb_js_abi_guest_rb_value_get(&recv);
-  struct rb_funcallv_thunk_ctx ctx = {.recv = abi_recv, .mid = mid, .args = args};
+  struct rb_funcallv_thunk_ctx ctx = {
+      .recv = abi_recv, .mid = mid, .args = args};
   RB_WASM_LIB_RT(retval = rb_protect(rb_funcallv_thunk, (VALUE)&ctx, ret1));
   // TODO(katei): protect the value from GC
   rb_gc_register_mark_object(retval);
   *ret0 = rb_js_abi_guest_rb_value_new((void *)retval);
 }
 
-rb_js_abi_guest_rb_id_t rb_js_abi_guest_rb_intern(rb_js_abi_guest_string_t *name) {
+rb_js_abi_guest_rb_id_t
+rb_js_abi_guest_rb_intern(rb_js_abi_guest_string_t *name) {
   return rb_intern(name->ptr);
 }
 
@@ -151,7 +160,8 @@ rb_js_abi_guest_rb_value_t rb_js_abi_guest_rb_errinfo(void) {
   return rb_js_abi_guest_rb_value_new((void *)retval);
 }
 
-void rb_js_abi_guest_rstring_ptr(rb_js_abi_guest_rb_value_t value, rb_js_abi_guest_string_t *ret0) {
+void rb_js_abi_guest_rstring_ptr(rb_js_abi_guest_rb_value_t value,
+                                 rb_js_abi_guest_string_t *ret0) {
   VALUE abi_value = (VALUE)rb_js_abi_guest_rb_value_get(&value);
   const char *str_ptr = (const char *)RSTRING_PTR(abi_value);
   rb_js_abi_guest_string_set(ret0, str_ptr);
