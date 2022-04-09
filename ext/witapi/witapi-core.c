774be71babab629fe36a0a6e8b539bde92e4e04f
Yuta Saito <kateinoigakukun@gmail.com>
2022-04-09 13:20:11 +0000

    Fix improper landing object protection from gc
    Ruby Hash uses Object#hash as key, and some objects returns non
    object-id as hash value. Therefore, when two objects with the same hash
    values are lent by JS-side, the arena protects only single object.
    This changes to use object-id as hash key explicitly.

--- ext/witapi/witapi-core.c
+++ ext/witapi/witapi-core.c
@@ -76,12 +76,16 @@ void *rb_wasm_handle_fiber_unwind(void (**new_fiber_entry)(void *, void *),
   }
 
 static VALUE rb_abi_guest_arena_hash;
+static VALUE rb_abi_guest_refcount_hash;
+
 static VALUE rb_abi_lend_object_internal(VALUE obj) {
-  VALUE ref_count = rb_hash_lookup(rb_abi_guest_arena_hash, obj);
+  VALUE object_id = rb_obj_id(obj);
+  VALUE ref_count = rb_hash_lookup(rb_abi_guest_refcount_hash, object_id);
   if (NIL_P(ref_count)) {
-    rb_hash_aset(rb_abi_guest_arena_hash, obj, INT2FIX(1));
+    rb_hash_aset(rb_abi_guest_arena_hash, object_id, obj);
+    rb_hash_aset(rb_abi_guest_refcount_hash, object_id, INT2FIX(1));
   } else {
-    rb_hash_aset(rb_abi_guest_arena_hash, obj, INT2FIX(FIX2INT(ref_count) + 1));
+    rb_hash_aset(rb_abi_guest_refcount_hash, object_id, INT2FIX(FIX2INT(ref_count) + 1));
   }
   return Qundef;
 }
@@ -93,17 +97,19 @@ static void rb_abi_lend_object(VALUE obj) {
 }
 
 static VALUE rb_abi_guest_rb_abi_value_dtor_internal(VALUE obj) {
-  VALUE ref_count = rb_hash_lookup(rb_abi_guest_arena_hash, obj);
+  VALUE object_id = rb_obj_id(obj);
+  VALUE ref_count = rb_hash_lookup(rb_abi_guest_refcount_hash, object_id);
   if (NIL_P(ref_count)) {
     rb_warning("rb_abi_guest_rb_abi_value_dtor: double free detected");
     return Qundef;
   }
   if (ref_count == INT2FIX(1)) {
-    rb_hash_delete(rb_abi_guest_arena_hash, obj);
     RB_WASM_DEBUG_LOG("rb_abi_guest_rb_abi_value_dtor: ref_count == 1\n");
+    rb_hash_delete(rb_abi_guest_refcount_hash, object_id);
+    rb_hash_delete(rb_abi_guest_arena_hash, object_id);
   } else {
-    rb_hash_aset(rb_abi_guest_arena_hash, obj, INT2FIX(FIX2INT(ref_count) - 1));
     RB_WASM_DEBUG_LOG("rb_abi_guest_rb_abi_value_dtor: ref_count = %d\n", FIX2INT(ref_count));
+    rb_hash_aset(rb_abi_guest_refcount_hash, object_id, INT2FIX(FIX2INT(ref_count) - 1));
   }
   return Qundef;
 }
@@ -125,7 +131,10 @@ void rb_abi_guest_ruby_init(void) {
   RB_WASM_LIB_RT(ruby_init())
 
   rb_abi_guest_arena_hash = rb_hash_new();
+  rb_abi_guest_refcount_hash = rb_hash_new();
+
   rb_gc_register_mark_object(rb_abi_guest_arena_hash);
+  rb_gc_register_mark_object(rb_abi_guest_refcount_hash);
 }
 
 void rb_abi_guest_ruby_sysinit(rb_abi_guest_list_string_t *args) {
