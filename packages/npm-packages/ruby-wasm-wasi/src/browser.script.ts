01a80b8f5a7fda29ecf5a6ddb1456f8b9b9a5396
Yuta Saito <kateinoigakukun@gmail.com>
2022-09-07 05:55:19 +0000

    load +stdlib variant by default for most use case

--- packages/npm-packages/ruby-wasm-wasi/src/browser.script.ts
+++ packages/npm-packages/ruby-wasm-wasi/src/browser.script.ts
@@ -2,7 +2,7 @@ import { DefaultRubyVM } from "./browser";
 
 export const main = async (pkg: { name: string, version: string }) => {
     const response = await fetch(
-        `https://cdn.jsdelivr.net/npm/${pkg.name}@${pkg.version}/dist/ruby.wasm`
+        `https://cdn.jsdelivr.net/npm/${pkg.name}@${pkg.version}/dist/ruby+stdlib.wasm`
     );
     const buffer = await response.arrayBuffer();
     const module = await WebAssembly.compile(buffer);
