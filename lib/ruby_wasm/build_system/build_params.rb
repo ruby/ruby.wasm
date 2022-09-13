8614d437266d2ca389ffb8a45db7a613b498b430
Yuta Saito <kateinoigakukun@gmail.com>
2022-09-13 07:56:35 +0000

    make user_exts configurable in task block

--- lib/ruby_wasm/build_system/build_params.rb
+++ lib/ruby_wasm/build_system/build_params.rb
@@ -2,11 +2,9 @@ module RubyWasm
   BuildParams =
     Struct.new(
       :name,
-      :src,
       :target,
       :debug,
       :default_exts,
-      :user_exts,
       :wasmoptflags,
       keyword_init: true
     )
