14ab3d09fbd94438bc930538d417f3709de60318
Yuta Saito <kateinoigakukun@gmail.com>
2022-09-13 07:41:48 +0000

    distinguish build_dir for product tasks

--- lib/ruby_wasm/build_system/product/libyaml.rb
+++ lib/ruby_wasm/build_system/product/libyaml.rb
@@ -26,11 +26,10 @@ module RubyWasm
     end
 
     def name
-      "libyaml-#{target}"
+      product_build_dir
     end
 
     def define_task
-      desc "build libyaml #{LIBYAML_VERSION} for #{target}"
       @install_task =
         task(name) do
           next if Dir.exist?(install_root)
