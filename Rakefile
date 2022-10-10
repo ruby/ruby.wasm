ruby/ruby.wasm

commit 15fff06f8806bc6e6c05826d95a55abe28e8134a

expose crossruby configuration options

Rakefile
@@ -51,12 +51,23 @@ namespace :build do
   BUILDS.each do |params|
     name = "#{params[:src]}-#{params[:target]}-#{params[:profile]}"
     source = BUILD_SOURCES[params[:src]].merge(name: params[:src])
+    debug = params[:debug]
     options = params
         .merge(BUILD_PROFILES[params[:profile]])
         .merge(src: source)
     options.delete :profile
     options.delete :user_exts
+    options.delete :debug
     RubyWasm::BuildTask.new(name, **options) do |t|
+      if debug
+        t.crossruby.debugflags = %w[-g]
+        t.crossruby.wasmoptflags = %w[-O3 -g]
+        t.crossruby.ldflags = %w[-Xlinker --stack-first -Xlinker -z -Xlinker stack-size=16777216]
+      else
+        t.crossruby.debugflags = %w[-g0]
+        t.crossruby.ldflags = %w[-Xlinker -zstack-size=16777216]
+      end
+
       toolchain = t.toolchain
       t.crossruby.user_exts = BUILD_PROFILES[params[:profile]][:user_exts].map do |ext|
         srcdir = File.join(LIB_ROOT, "ext", ext)

lib/ruby_wasm/build_system/build_params.rb
@@ -1,11 +1,3 @@
 module RubyWasm
-  BuildParams =
-    Struct.new(
-      :name,
-      :target,
-      :debug,
-      :default_exts,
-      :wasmoptflags,
-      keyword_init: true
-    )
+  BuildParams = Struct.new(:name, :target, :default_exts, keyword_init: true)
 end

lib/ruby_wasm/build_system/product/crossruby.rb
@@ -79,7 +79,14 @@ module RubyWasm
 
   class CrossRubyProduct < AutoconfProduct
     attr_reader :source, :toolchain, :build, :configure
-    attr_accessor :user_exts, :wasmoptflags
+    attr_accessor :user_exts,
+                  :wasmoptflags,
+                  :cppflags,
+                  :cflags,
+                  :ldflags,
+                  :debugflags,
+                  :xcflags,
+                  :xldflags
 
     def initialize(
       params,
@@ -98,7 +105,13 @@ module RubyWasm
       @toolchain = toolchain
       @dep_tasks = []
       @user_exts = user_exts
-      @wasmoptflags = nil
+      @wasmoptflags = []
+      @cppflags = []
+      @cflags = []
+      @ldflags = []
+      @debugflags = []
+      @xcflags = []
+      @xldflags = []
       super(@params.target, @toolchain)
     end
 
@@ -200,15 +213,8 @@ module RubyWasm
       target = @params.target
       default_exts = @params.default_exts
 
-      ldflags =
-        if @params.debug
-          # use --stack-first to detect stack overflow easily
-          %w[-Xlinker --stack-first -Xlinker -z -Xlinker stack-size=16777216]
-        else
-          %w[-Xlinker -zstack-size=16777216]
-        end
-
-      xldflags = []
+      ldflags = @ldflags.dup
+      xldflags = @xldflags.dup
 
       args = self.system_triplet_args + ["--build", build_triple]
       args << "--with-static-linked-ext"
@@ -231,7 +237,7 @@ module RubyWasm
       (@user_exts || []).each { |lib| xldflags << "@#{lib.linklist(self)}" }
       xldflags << extinit_obj
 
-      xcflags = []
+      xcflags = @xcflags.dup
       xcflags << "-DWASM_SETJMP_STACK_BUFFER_SIZE=24576"
       xcflags << "-DWASM_FIBER_STACK_BUFFER_SIZE=24576"
       xcflags << "-DWASM_SCAN_STACK_BUFFER_SIZE=24576"
@@ -239,12 +245,10 @@ module RubyWasm
       args << %Q(LDFLAGS="#{ldflags.join(" ")}")
       args << %Q(XLDFLAGS="#{xldflags.join(" ")}")
       args << %Q(XCFLAGS="#{xcflags.join(" ")}")
-      if @params.debug
-        args << %Q(debugflags="-g")
-        args << %Q(wasmoptflags="-O3 -g")
-      else
-        args << %Q(debugflags="-g0")
-        args << %Q(wasmoptflags="#{wasmoptflags}") if @wasmoptflags
+      args << %Q(debugflags="#{@debugflags.join(" ")}")
+      args << %Q(cppflags="#{@cppflags.join(" ")}")
+      unless wasmoptflags.empty?
+        args << %Q(wasmoptflags="#{@wasmoptflags.join(" ")}")
       end
       args << "--disable-install-doc"
       args

commit d14dc20379fdbc94a5242ee91e3f35ba0dfe1d93

Fix debug build param source
It's broken since 15fff06f8806bc6e6c05826d95a55abe28e8134a

Rakefile
@@ -51,10 +51,10 @@ namespace :build do
   BUILDS.each do |params|
     name = "#{params[:src]}-#{params[:target]}-#{params[:profile]}"
     source = BUILD_SOURCES[params[:src]].merge(name: params[:src])
-    debug = params[:debug]
     options = params
         .merge(BUILD_PROFILES[params[:profile]])
         .merge(src: source)
+    debug = options[:debug]
     options.delete :profile
     options.delete :user_exts
     options.delete :debug