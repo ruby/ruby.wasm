f3d515216dcb3325595132c351c72fa90a77a17c
Yuta Saito <kateinoigakukun@gmail.com>
2022-10-12 06:36:36 +0000

    Move JS module rdoc into .rb file to include example section
    rdoc somehow ignore the section in .c

--- ext/js/lib/js.rb
+++ ext/js/lib/js.rb
@@ -1,5 +1,20 @@
 require "js.so"
 
+# The JS module provides a way to interact with JavaScript from Ruby.
+#
+# == Example
+#
+#   require 'js'
+#   JS.eval("return 1 + 2") # => 3
+#   JS.global[:document].write("Hello, world!")
+#   JS.global[:document].addEventListner("click") do |event|
+#     puts event          # => # [object MouseEvent]
+#     puts event[:detail] # => 1
+#   end
+#
+module JS
+end
+
 class JS::Object
   def method_missing(sym, *args, &block)
     if self[sym].typeof == "function"
