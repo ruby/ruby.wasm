3161aa95315cb35984a97fdba996baf58cb6e7da
Yuta Saito <kateinoigakukun@gmail.com>
2022-10-17 08:13:29 +0000

    Allow to specify already existing gid also
    Emscripten image has two non-root users and groups: emscripten (1000),
    me (1001). Since default user is me (1001), we need to pass host uid/gid
    when host uid/gid are 1000. But passing them caused:
    `groupmod: GID '1000' already exists`
    so allow duplicated gid as well.

--- builders/wasm32-unknown-wasi/entrypoint.sh
+++ builders/wasm32-unknown-wasi/entrypoint.sh
@@ -3,5 +3,5 @@
 set -e
 
 [ ! -z "${RUBYWASM_UID+x}" ] && usermod --uid "$RUBYWASM_UID" --non-unique me
-[ ! -z "${RUBYWASM_GID+x}" ] && groupmod --gid "$RUBYWASM_GID" me
+[ ! -z "${RUBYWASM_GID+x}" ] && groupmod --gid "$RUBYWASM_GID" --non-unique me
 exec gosu me "$@"
