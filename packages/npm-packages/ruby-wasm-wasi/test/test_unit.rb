6a9585d6e4695bf905b1b8b048c4d784054b7412
Yuta Saito <kateinoigakukun@gmail.com>
2022-11-05 10:50:26 +0000

    Add basic unit and integration tests for promise scheduler

--- packages/npm-packages/ruby-wasm-wasi/test/test_unit.rb
+++ packages/npm-packages/ruby-wasm-wasi/test/test_unit.rb
@@ -1,3 +1,4 @@
 require_relative "./unit/test_js"
 require_relative "./unit/test_object"
 require_relative "./unit/test_error"
+require_relative "./unit/test_async"
--- packages/npm-packages/ruby-wasm-wasi/test/vm.test.ts
+++ packages/npm-packages/ruby-wasm-wasi/test/vm.test.ts
@@ -191,4 +191,33 @@ eval:11:in \`<main>'`);
     const hash = vm.eval(`Hash.new`);
     hash.call("store", vm.eval(`"key1"`), vm.wrap(new Object()));
   });
+
+  test("async eval over microtasks", async () => {
+    const vm = await initRubyVM();
+    const result = await vm.evalAsync(`
+    require 'js'
+    o = JS.eval(<<~JS)
+      return {
+        async_func: () => {
+          return new Promise((resolve) => {
+            queueMicrotask(() => {
+              resolve(42)
+            });
+          });
+        }
+      }
+    JS
+    o.async_func.await
+    `);
+    expect(result.toString()).toBe("42");
+  });
+
+  test("async eval multiple times", async () => {
+    const vm = await initRubyVM();
+    vm.eval(`require "js"`);
+    const ret0 = await vm.evalAsync(`JS.global[:Promise].resolve(42).await`);
+    expect(ret0.toString()).toBe("42");
+    const ret1 = await vm.evalAsync(`JS.global[:Promise].resolve(43).await`);
+    expect(ret1.toString()).toBe("43");
+  });
 });
