9c9265d2b67d58129a1fb75378f3e96fa1ac38ad
David Cameron <david.cameron@shopify.com>
2022-12-06 13:24:19 -0500

    Remove a number of witapi elements

--- ext/witapi/bindgen/rb-abi-guest.c
+++ ext/witapi/bindgen/rb-abi-guest.c
@@ -1,86 +1,6 @@
 #include <stdlib.h>
 #include <rb-abi-guest.h>
 
-__attribute__((weak, export_name("cabi_realloc")))
-void *cabi_realloc(
-void *ptr,
-size_t orig_size,
-size_t org_align,
-size_t new_size
-) {
-  void *ret = realloc(ptr, new_size);
-  if (!ret)
-  abort();
-  return ret;
-}
-
-__attribute__((import_module("canonical_abi"), import_name("resource_drop_rb-iseq")))
-void __resource_rb_iseq_drop(uint32_t idx);
-
-void rb_abi_guest_rb_iseq_free(rb_abi_guest_rb_iseq_t *ptr) {
-  __resource_rb_iseq_drop(ptr->idx);
-}
-
-__attribute__((import_module("canonical_abi"), import_name("resource_clone_rb-iseq")))
-uint32_t __resource_rb_iseq_clone(uint32_t idx);
-
-rb_abi_guest_rb_iseq_t rb_abi_guest_rb_iseq_clone(rb_abi_guest_rb_iseq_t *ptr) {
-  return (rb_abi_guest_rb_iseq_t){__resource_rb_iseq_clone(ptr->idx)};
-}
-
-__attribute__((import_module("canonical_abi"), import_name("resource_new_rb-iseq")))
-uint32_t __resource_rb_iseq_new(uint32_t val);
-
-rb_abi_guest_rb_iseq_t rb_abi_guest_rb_iseq_new(void *data) {
-  return (rb_abi_guest_rb_iseq_t){__resource_rb_iseq_new((uint32_t) data)};
-}
-
-__attribute__((import_module("canonical_abi"), import_name("resource_get_rb-iseq")))
-uint32_t __resource_rb_iseq_get(uint32_t idx);
-
-void* rb_abi_guest_rb_iseq_get(rb_abi_guest_rb_iseq_t *ptr) {
-  return (void*) __resource_rb_iseq_get(ptr->idx);
-}
-
-__attribute__((export_name("canonical_abi_drop_rb-iseq")))
-void __resource_rb_iseq_dtor(uint32_t val) {
-  if (rb_abi_guest_rb_iseq_dtor)
-  rb_abi_guest_rb_iseq_dtor((void*) val);
-}
-
-__attribute__((import_module("canonical_abi"), import_name("resource_drop_rb-abi-value")))
-void __resource_rb_abi_value_drop(uint32_t idx);
-
-void rb_abi_guest_rb_abi_value_free(rb_abi_guest_rb_abi_value_t *ptr) {
-  __resource_rb_abi_value_drop(ptr->idx);
-}
-
-__attribute__((import_module("canonical_abi"), import_name("resource_clone_rb-abi-value")))
-uint32_t __resource_rb_abi_value_clone(uint32_t idx);
-
-rb_abi_guest_rb_abi_value_t rb_abi_guest_rb_abi_value_clone(rb_abi_guest_rb_abi_value_t *ptr) {
-  return (rb_abi_guest_rb_abi_value_t){__resource_rb_abi_value_clone(ptr->idx)};
-}
-
-__attribute__((import_module("canonical_abi"), import_name("resource_new_rb-abi-value")))
-uint32_t __resource_rb_abi_value_new(uint32_t val);
-
-rb_abi_guest_rb_abi_value_t rb_abi_guest_rb_abi_value_new(void *data) {
-  return (rb_abi_guest_rb_abi_value_t){__resource_rb_abi_value_new((uint32_t) data)};
-}
-
-__attribute__((import_module("canonical_abi"), import_name("resource_get_rb-abi-value")))
-uint32_t __resource_rb_abi_value_get(uint32_t idx);
-
-void* rb_abi_guest_rb_abi_value_get(rb_abi_guest_rb_abi_value_t *ptr) {
-  return (void*) __resource_rb_abi_value_get(ptr->idx);
-}
-
-__attribute__((export_name("canonical_abi_drop_rb-abi-value")))
-void __resource_rb_abi_value_dtor(uint32_t val) {
-  if (rb_abi_guest_rb_abi_value_dtor)
-  rb_abi_guest_rb_abi_value_dtor((void*) val);
-}
 #include <string.h>
 
 void rb_abi_guest_string_set(rb_abi_guest_string_t *ret, const char *s) {
@@ -101,25 +21,6 @@ void rb_abi_guest_string_free(rb_abi_guest_string_t *ret) {
   ret->ptr = NULL;
   ret->len = 0;
 }
-void rb_abi_guest_list_string_free(rb_abi_guest_list_string_t *ptr) {
-  for (size_t i = 0; i < ptr->len; i++) {
-    rb_abi_guest_string_free(&ptr->ptr[i]);
-  }
-  if (ptr->len > 0) {
-    free(ptr->ptr);
-  }
-}
-void rb_abi_guest_tuple2_rb_abi_value_s32_free(rb_abi_guest_tuple2_rb_abi_value_s32_t *ptr) {
-  rb_abi_guest_rb_abi_value_free(&ptr->f0);
-}
-void rb_abi_guest_list_rb_abi_value_free(rb_abi_guest_list_rb_abi_value_t *ptr) {
-  for (size_t i = 0; i < ptr->len; i++) {
-    rb_abi_guest_rb_abi_value_free(&ptr->ptr[i]);
-  }
-  if (ptr->len > 0) {
-    free(ptr->ptr);
-  }
-}
 
 __attribute__((aligned(4)))
 static uint8_t RET_AREA[8];
@@ -136,12 +37,6 @@ void __wasm_export_rb_abi_guest_ruby_sysinit(int32_t arg, int32_t arg0) {
   rb_abi_guest_list_string_t arg1 = (rb_abi_guest_list_string_t) { (rb_abi_guest_string_t*)(arg), (size_t)(arg0) };
   rb_abi_guest_ruby_sysinit(&arg1);
 }
-__attribute__((export_name("ruby-options: func(args: list<string>) -> handle<rb-iseq>")))
-int32_t __wasm_export_rb_abi_guest_ruby_options(int32_t arg, int32_t arg0) {
-  rb_abi_guest_list_string_t arg1 = (rb_abi_guest_list_string_t) { (rb_abi_guest_string_t*)(arg), (size_t)(arg0) };
-  rb_abi_guest_rb_iseq_t ret = rb_abi_guest_ruby_options(&arg1);
-  return (ret).idx;
-}
 __attribute__((export_name("ruby-script: func(name: string) -> ()")))
 void __wasm_export_rb_abi_guest_ruby_script(int32_t arg, int32_t arg0) {
   rb_abi_guest_string_t arg1 = (rb_abi_guest_string_t) { (char*)(arg), (size_t)(arg0) };
@@ -150,58 +45,4 @@ void __wasm_export_rb_abi_guest_ruby_script(int32_t arg, int32_t arg0) {
 __attribute__((export_name("ruby-init-loadpath: func() -> ()")))
 void __wasm_export_rb_abi_guest_ruby_init_loadpath(void) {
   rb_abi_guest_ruby_init_loadpath();
-}
-__attribute__((export_name("rb-eval-string-protect: func(str: string) -> tuple<handle<rb-abi-value>, s32>")))
-int32_t __wasm_export_rb_abi_guest_rb_eval_string_protect(int32_t arg, int32_t arg0) {
-  rb_abi_guest_string_t arg1 = (rb_abi_guest_string_t) { (char*)(arg), (size_t)(arg0) };
-  rb_abi_guest_tuple2_rb_abi_value_s32_t ret;
-  rb_abi_guest_rb_eval_string_protect(&arg1, &ret);
-  int32_t ptr = (int32_t) &RET_AREA;
-  *((int32_t*)(ptr + 0)) = ((ret).f0).idx;
-  *((int32_t*)(ptr + 4)) = (ret).f1;
-  return ptr;
-}
-__attribute__((export_name("rb-funcallv-protect: func(recv: handle<rb-abi-value>, mid: u32, args: list<handle<rb-abi-value>>) -> tuple<handle<rb-abi-value>, s32>")))
-int32_t __wasm_export_rb_abi_guest_rb_funcallv_protect(int32_t arg, int32_t arg0, int32_t arg1, int32_t arg2) {
-  rb_abi_guest_list_rb_abi_value_t arg3 = (rb_abi_guest_list_rb_abi_value_t) { (rb_abi_guest_rb_abi_value_t*)(arg1), (size_t)(arg2) };
-  rb_abi_guest_tuple2_rb_abi_value_s32_t ret;
-  rb_abi_guest_rb_funcallv_protect((rb_abi_guest_rb_abi_value_t){ arg }, (uint32_t) (arg0), &arg3, &ret);
-  int32_t ptr = (int32_t) &RET_AREA;
-  *((int32_t*)(ptr + 0)) = ((ret).f0).idx;
-  *((int32_t*)(ptr + 4)) = (ret).f1;
-  return ptr;
-}
-__attribute__((export_name("rb-intern: func(name: string) -> u32")))
-int32_t __wasm_export_rb_abi_guest_rb_intern(int32_t arg, int32_t arg0) {
-  rb_abi_guest_string_t arg1 = (rb_abi_guest_string_t) { (char*)(arg), (size_t)(arg0) };
-  rb_abi_guest_rb_id_t ret = rb_abi_guest_rb_intern(&arg1);
-  return (int32_t) (ret);
-}
-__attribute__((export_name("rb-errinfo: func() -> handle<rb-abi-value>")))
-int32_t __wasm_export_rb_abi_guest_rb_errinfo(void) {
-  rb_abi_guest_rb_abi_value_t ret = rb_abi_guest_rb_errinfo();
-  return (ret).idx;
-}
-__attribute__((export_name("rb-clear-errinfo: func() -> ()")))
-void __wasm_export_rb_abi_guest_rb_clear_errinfo(void) {
-  rb_abi_guest_rb_clear_errinfo();
-}
-__attribute__((export_name("rstring-ptr: func(value: handle<rb-abi-value>) -> string")))
-int32_t __wasm_export_rb_abi_guest_rstring_ptr(int32_t arg) {
-  rb_abi_guest_string_t ret;
-  rb_abi_guest_rstring_ptr((rb_abi_guest_rb_abi_value_t){ arg }, &ret);
-  int32_t ptr = (int32_t) &RET_AREA;
-  *((int32_t*)(ptr + 4)) = (int32_t) (ret).len;
-  *((int32_t*)(ptr + 0)) = (int32_t) (ret).ptr;
-  return ptr;
-}
-__attribute__((export_name("cabi_post_rstring-ptr")))
-void __wasm_export_rb_abi_guest_rstring_ptr_post_return(int32_t arg0) {
-  if ((*((int32_t*) (arg0 + 4))) > 0) {
-    free((void*) (*((int32_t*) (arg0 + 0))));
-  }
-}
-__attribute__((export_name("rb-vm-bugreport: func() -> ()")))
-void __wasm_export_rb_abi_guest_rb_vm_bugreport(void) {
-  rb_abi_guest_rb_vm_bugreport();
 }
