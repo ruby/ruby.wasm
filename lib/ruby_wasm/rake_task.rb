04189071bbeab89aaac22450471051b490ae69a1
Yuta Saito <kateinoigakukun@gmail.com>
2023-02-01 03:04:11 +0000

    rake format

--- lib/ruby_wasm/rake_task.rb
+++ lib/ruby_wasm/rake_task.rb
@@ -52,7 +52,8 @@ class RubyWasm::BuildTask < ::Rake::TaskLib
     @baseruby = RubyWasm::BaseRubyProduct.new(@build_dir, @source)
     @openssl = RubyWasm::OpenSSLProduct.new(@build_dir, @target, @toolchain)
 
-    build_params = RubyWasm::BuildParams.new(options.merge(name: name, target: target))
+    build_params =
+      RubyWasm::BuildParams.new(options.merge(name: name, target: target))
 
     @crossruby =
       RubyWasm::CrossRubyProduct.new(
