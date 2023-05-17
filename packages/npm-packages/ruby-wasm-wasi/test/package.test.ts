ac38a84a3a8abc7f55ab7274f5a2c9a998f7dad6
Yuta Saito <kateinoigakukun@gmail.com>
2023-05-17 13:43:22 +0000

    Add RUBY_ROOT hook point in jest test

--- packages/npm-packages/ruby-wasm-wasi/test/package.test.ts
+++ packages/npm-packages/ruby-wasm-wasi/test/package.test.ts
@@ -2,6 +2,7 @@ import fs from "fs/promises";
 import path from "path";
 import { WASI } from "wasi";
 import { RubyVM } from "../dist/index.umd.js";
+import { DefaultRubyVM } from "../src/node";
 
 const initRubyVM = async (rubyModule: WebAssembly.Module, args: string[]) => {
   const wasi = new WASI();
@@ -29,12 +30,28 @@ const initRubyVM = async (rubyModule: WebAssembly.Module, args: string[]) => {
 describe("Packaging validation", () => {
   jest.setTimeout(20 /*sec*/ * 1000);
 
+  const moduleCache = new Map<string, WebAssembly.Module>();
+  const loadWasmModule = async (file: string) => {
+    if (moduleCache.has(file)) {
+      return moduleCache.get(file)!;
+    }
+    const binary = await fs.readFile(path.join(__dirname, `./../dist/${file}`));
+    const mod = await WebAssembly.compile(binary.buffer);
+    moduleCache.set(file, mod);
+    return mod;
+  };
+
+  test("DefaultRubyVM", async () => {
+    const mod = await loadWasmModule(`ruby+stdlib.wasm`);
+    const { vm } = await DefaultRubyVM(mod);
+    vm.eval(`require "stringio"`);
+  })
+
   test.each([
     { file: "ruby+stdlib.wasm", stdlib: true },
     { file: "ruby.debug+stdlib.wasm", stdlib: true },
   ])("Load all variants", async ({ file, stdlib }) => {
-    const binary = await fs.readFile(path.join(__dirname, `./../dist/${file}`));
-    const mod = await WebAssembly.compile(binary.buffer);
+    const mod = await loadWasmModule(file);
     const { vm } = await initRubyVM(mod, ["ruby.wasm", "-e_=0"]);
     // Check loading ext library
     vm.eval(`require "stringio"`);
@@ -45,10 +62,7 @@ describe("Packaging validation", () => {
   });
 
   test("ruby.debug+stdlib.wasm has debug info", async () => {
-    const binary = await fs.readFile(
-      path.join(__dirname, `./../dist/ruby.debug+stdlib.wasm`)
-    );
-    const mod = await WebAssembly.compile(binary.buffer);
+    const mod = await loadWasmModule("ruby.debug+stdlib.wasm");
     const nameSections = WebAssembly.Module.customSections(mod, "name");
     expect(nameSections.length).toBe(1);
   });
