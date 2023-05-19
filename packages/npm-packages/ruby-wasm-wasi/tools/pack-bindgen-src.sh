62ad9730f7b45944cdc11f129035f64aab8cde45
Yuta Saito <kateinoigakukun@gmail.com>
2023-05-19 17:23:57 +0000

    Move dist/bindgen to @ruby/wasm-wasi pkg

--- packages/npm-packages/ruby-wasm-wasi/tools/pack-bindgen-src.sh
+++ packages/npm-packages/ruby-wasm-wasi/tools/pack-bindgen-src.sh
@@ -8,9 +8,6 @@ fi
 
 package_dir="$(cd "$(dirname "$0")/.." && pwd)"
 dist_dir="$1"
-repo_dir="$package_dir/../../../"
 
-mkdir -p "$dist_dir"
-
-cp "$repo_dir/LICENSE" "$dist_dir/LICENSE"
-cp "$repo_dir/NOTICE" "$dist_dir/NOTICE"
+mkdir -p "$dist_dir/bindgen"
+find "$package_dir/src/bindgen" -name "*.d.ts" -exec cp {} "$dist_dir/bindgen" \;
