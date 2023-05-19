679832914c5e131e475719823246071d3c243808
Yuta Saito <kateinoigakukun@gmail.com>
2023-05-19 15:48:08 +0000

    Remove ruby-wasm-wasi/build-package.sh

--- packages/npm-packages/ruby-head-wasm-wasi/build-package.sh
+++ packages/npm-packages/ruby-head-wasm-wasi/build-package.sh
@@ -14,13 +14,8 @@ ruby_root="$1"
 package_dir="$(cd "$(dirname "$0")" && pwd)"
 base_package_dir="$package_dir/../ruby-wasm-wasi"
 dist_dir="$package_dir/dist"
-repo_dir="$package_dir/../../../"
 
 rm -rf "$dist_dir"
-(
-  cd "$base_package_dir" && \
-  ./build-package.sh "$ruby_root"
-)
 set -ex
 (cd "$package_dir" && npm run build)
 cp -R "$base_package_dir/dist/." "$dist_dir"
