4102e124adfc6497b82656d347039f3a879866c6
Yuta Saito <kateinoigakukun@gmail.com>
2023-05-19 15:15:33 +0000

    Resolve ruby-wasm-wasi by node_modules import

--- packages/npm-packages/ruby-head-wasm-wasi/src/browser.script.js
+++ packages/npm-packages/ruby-head-wasm-wasi/src/browser.script.js
@@ -1,5 +1,5 @@
 
-import { main } from "../../ruby-wasm-wasi/dist/browser.script.esm"
+import { main } from "ruby-wasm-wasi/dist/browser.script.esm"
 import * as pkg from "../package.json"
 
 main(pkg)
