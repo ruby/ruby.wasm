2c0f150d908a2bea023153da69426344839efb1e
Yuta Saito <kateinoigakukun@gmail.com>
2023-07-28 14:55:17 +0000

    Skip e2e tests with CDN assets when the version is not released yet

--- packages/npm-packages/ruby-wasm-wasi/test-e2e/examples/examples.spec.ts
+++ packages/npm-packages/ruby-wasm-wasi/test-e2e/examples/examples.spec.ts
@@ -1,13 +1,27 @@
 import { test, expect, Page } from '@playwright/test';
 import path from "path"
 import { waitForRubyVM, setupDebugLog, setupProxy } from "../support"
+import { readFileSync } from 'fs';
+import http from 'http';
+import https from 'https';
 
 test.beforeEach(async ({ context }) => {
   setupDebugLog(context);
   if (process.env.RUBY_NPM_PACKAGE_ROOT) {
     setupProxy(context);
   } else {
-    console.info("Testing against CDN deployed files")
+    console.info("Testing against CDN deployed files");
+    const packagePath = path.join(__dirname, "..", "..", "package.json")
+    const packageInfo = JSON.parse(readFileSync(packagePath, "utf-8"))
+    const version = packageInfo.version
+    const url = `https://registry.npmjs.org/ruby-head-wasm-wasi/${version}`
+    const response = await new Promise<http.IncomingMessage>((resolve, reject) => {
+      https.get(url, resolve).on("error", reject)
+    })
+    if (response.statusCode == 404) {
+      console.log(`ruby-head-wasm-wasi@${version} is not published yet, so skipping CDN tests`);
+      test.skip();
+    }
   }
 })
 
