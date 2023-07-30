57b59f93748db1f1ceb8f6082ccad33d1f7f7dd4
Yuta Saito <kateinoigakukun@gmail.com>
2023-07-30 07:42:07 +0000

    rake format

--- ext/js/lib/js/hash.rb
+++ ext/js/lib/js/hash.rb
@@ -2,9 +2,7 @@ class Hash
   # Convert a hash to a JavaScript object
   def to_js
     new_object = JS.eval("return {}")
-    self.each do |key, value|
-      new_object[key] = value
-    end
+    self.each { |key, value| new_object[key] = value }
     new_object
   end
 end
