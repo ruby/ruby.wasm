c0be5fd237a7dff166844c03c166be35b3796c28
krmbn0576 <krmbn0576@gmail.com>
2023-09-21 10:36:32 +0900

    Improve `Array#to_js` speed
    Convert Ruby elements to JS elements while preparing JS method call
    under `JS::Object#call` instead of converting at Ruby level for each
    element.

--- ext/js/lib/js/array.rb
+++ ext/js/lib/js/array.rb
@@ -2,10 +2,8 @@ class Array
   # Convert Ruby array to JavaScript array
   def to_js
     new_array = JS.eval("return []")
-    self.each do |element|
-      # NOTE: This method call implicitly convert element to JS object by to_js
-      new_array.push element
-    end
+    # NOTE: This method call implicitly convert element to JS object by to_js
+    new_array.push *self
     new_array
   end
 end
