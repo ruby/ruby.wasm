16c829d41b9f70b43a15c149034f74259218eb20
Yuta Saito <kateinoigakukun@gmail.com>
2023-12-14 13:12:58 +0000

    Remove workaround for `@bjorn3/browser_wasi_shim`
    Those issues are now fixed in the upstream `@bjorn3/browser_wasi_shim`

--- packages/npm-packages/ruby-wasm-wasi/src/browser.ts
+++ packages/npm-packages/ruby-wasm-wasi/src/browser.ts
@@ -28,30 +28,6 @@ export const DefaultRubyVM = async (
   const printer = options.consolePrint ?? true ? consolePrinter() : undefined;
   printer?.addToImports(imports);
 
-  {
-    // WORKAROUND: browser_wasi_shim does not support some syscalls yet
-    // and returns -1 instead of proper ERRNO values and it results in confusing
-    // error messages "Success -- /path/to/file".
-    // Update browser_wasi_shim version when my fix[^1] will be released.
-    // [^1]: https://github.com/bjorn3/browser_wasi_shim/commit/6193f7482633ef818604375d9755ded67946adfc
-    const syscalls = imports["wasi_snapshot_preview1"];
-    for (const name of Object.keys(syscalls)) {
-      const original = syscalls[name];
-      syscalls[name] = (...args) => {
-        const result = original(...args);
-        if (result === -1) {
-          return 58; // ENOTSUP
-        }
-        // browser_wasi_shim's `random_get` returns `undefined` on success
-        // instead of `0`.
-        if (result === undefined) {
-          return 0; // Success
-        }
-        return result;
-      };
-    }
-  }
-
   const instance = await WebAssembly.instantiate(rubyModule, imports);
   await vm.setInstance(instance);
 
