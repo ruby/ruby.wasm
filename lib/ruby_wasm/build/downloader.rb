208127e8b07bd7b3ce248db76faf28241065233f
krmbn0576 <krmbn0576@gmail.com>
2024-01-19 16:47:25 +0900

    Fix broken prebuilt_downloader

--- lib/ruby_wasm/build/downloader.rb
+++ lib/ruby_wasm/build/downloader.rb
@@ -1,5 +1,15 @@
 module RubyWasm
   class Downloader
+    def format_size(size)
+      units = %w[B KB MB GB TB]
+      unit = 0
+      while size > 1024 and unit < units.size - 1
+        size /= 1024.0
+        unit += 1
+      end
+      "%s #{units[unit]}" % size.round(2)
+    end
+
     def download(url, dest, message)
       require "open-uri"
       content_length = 0
@@ -8,7 +18,7 @@ module RubyWasm
         uri,
         content_length_proc: ->(len) { content_length = len },
         progress_proc: ->(size) do
-          print "\r#{message} (#{SizeFormatter.format(content_length)}) %.2f%%" %
+          print "\r#{message} (#{format_size(content_length)}) %.2f%%" %
                   (size.to_f / content_length * 100)
         end
       ) { |f| File.open(dest, "wb") { |out| out.write f.read } }
