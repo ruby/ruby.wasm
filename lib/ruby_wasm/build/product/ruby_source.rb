ruby/ruby.wasm

commit e5fa660dc2342ee6496d658c1a5a2c79684da3d9

build: Split command arguments at Ruby level

lib/ruby_wasm/build/product/ruby_source.rb
@@ -36,14 +36,26 @@ module RubyWasm
       when "github"
         repo_url = "https://github.com/#{@params[:repo]}.git"
         executor.mkdir_p src_dir
-        executor.system "git init", chdir: src_dir
-        executor.system "git remote add origin #{repo_url}", chdir: src_dir
+        executor.system "git", "init", chdir: src_dir
+        executor.system "git",
+                        "remote",
+                        "add",
+                        "origin",
+                        repo_url,
+                        chdir: src_dir
         executor.system(
-          "git fetch --depth 1 origin #{@params[:rev]}:origin/#{@params[:rev]}",
+          "git",
+          "fetch",
+          "--depth",
+          "1",
+          "origin",
+          "#{@params[:rev]}:origin/#{@params[:rev]}",
           chdir: src_dir
         ) or raise "failed to clone #{repo_url}"
         executor.system(
-          "git checkout origin/#{@params[:rev]}",
+          "git",
+          "checkout",
+          "origin/#{@params[:rev]}",
           chdir: src_dir
         ) or raise "failed to checkout #{@params[:rev]}"
       when "local"
@@ -53,7 +65,7 @@ module RubyWasm
         raise "unknown source type: #{@params[:type]}"
       end
       (@params[:patches] || []).each do |patch_path|
-        executor.system "patch -p1 < #{patch_path}", chdir: src_dir
+        executor.system "patch", "-p1", patch_path, chdir: src_dir
       end
     end
 
@@ -61,7 +73,14 @@ module RubyWasm
       fetch(executor) unless File.exist?(src_dir)
       unless File.exist?(configure_file)
         Dir.chdir(src_dir) do
-          executor.system "ruby tool/downloader.rb -d tool -e gnu config.guess config.sub" or
+          executor.system "ruby",
+                          "tool/downloader.rb",
+                          "-d",
+                          "tool",
+                          "-e",
+                          "gnu",
+                          "config.guess",
+                          "config.sub" or
             raise "failed to download config.guess and config.sub"
           executor.system "./autogen.sh" or raise "failed to run autogen.sh"
         end

lib/ruby_wasm/build/product/openssl.rb
@@ -59,14 +59,28 @@ module RubyWasm
 
       executor.mkdir_p File.dirname(product_build_dir)
       executor.rm_rf product_build_dir
-      executor.system "curl -L https://www.openssl.org/source/openssl-#{OPENSSL_VERSION}.tar.gz | tar xz",
-                      chdir: File.dirname(product_build_dir)
+      executor.mkdir_p product_build_dir
+      tarball_path =
+        File.join(product_build_dir, "openssl-#{OPENSSL_VERSION}.tar.gz")
+      executor.system "curl",
+                      "-o",
+                      tarball_path,
+                      "-L",
+                      "https://www.openssl.org/source/openssl-#{OPENSSL_VERSION}.tar.gz"
+      executor.system "tar",
+                      "xzf",
+                      tarball_path,
+                      "-C",
+                      product_build_dir,
+                      "--strip-components=1"
 
-      executor.system "./Configure #{configure_args.join(" ")}",
-                      chdir: product_build_dir
+      executor.system "./Configure", *configure_args, chdir: product_build_dir
       # Use "install_sw" instead of "install" because it tries to install docs and it's very slow.
       # OpenSSL build system doesn't have well support for parallel build, so force -j1.
-      executor.system "make -j1 install_sw DESTDIR=#{destdir}",
+      executor.system "make",
+                      "-j1",
+                      "install_sw",
+                      "DESTDIR=#{destdir}",
                       chdir: product_build_dir
     end
   end

lib/ruby_wasm/build/product/zlib.rb
@@ -39,13 +39,29 @@ module RubyWasm
 
       executor.mkdir_p File.dirname(product_build_dir)
       executor.rm_rf product_build_dir
+      executor.mkdir_p product_build_dir
 
-      executor.system "curl -L https://zlib.net/zlib-#{ZLIB_VERSION}.tar.gz | tar xz",
-                      chdir: File.dirname(product_build_dir)
+      tarball_path = File.join(product_build_dir, "zlib-#{ZLIB_VERSION}.tar.gz")
+      executor.system "curl",
+                      "-o",
+                      tarball_path,
+                      "-L",
+                      "https://zlib.net/zlib-#{ZLIB_VERSION}.tar.gz"
+      executor.system "tar",
+                      "xzf",
+                      tarball_path,
+                      "-C",
+                      product_build_dir,
+                      "--strip-components=1"
 
-      executor.system "#{configure_args.join(" ")} ./configure --static",
+      executor.system "env",
+                      *configure_args,
+                      "./configure",
+                      "--static",
                       chdir: product_build_dir
-      executor.system "make install DESTDIR=#{destdir}",
+      executor.system "make",
+                      "install",
+                      "DESTDIR=#{destdir}",
                       chdir: product_build_dir
     end
   end

commit e10aeaf0ca9d8c38c1ed94e95e64e40132667a53

Fix patch command invocation
It has been broken since e5fa660dc2342ee6496d658c1a5a2c79684da3d9 :(

lib/ruby_wasm/build/product/ruby_source.rb
@@ -80,7 +80,7 @@ module RubyWasm
         raise "unknown source type: #{@params[:type]}"
       end
       (@params[:patches] || []).each do |patch_path|
-        executor.system "patch", "-p1", patch_path, chdir: src_dir
+        executor.system "patch", "-p1", "-i", patch_path, chdir: src_dir
       end
     end
 