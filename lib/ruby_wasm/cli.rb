bfa84d87b131591e4490c12b9c71320dfa01e03a
Yuta Saito <kateinoigakukun@gmail.com>
2024-03-27 02:15:56 +0000

    build: Keep default extensions for older Ruby versions

--- lib/ruby_wasm/cli.rb
+++ lib/ruby_wasm/cli.rb
@@ -162,7 +162,7 @@ module RubyWasm
       config = { target: options[:target_triplet], src: compute_build_source(options) }
       case options[:profile]
       when "full"
-        config[:default_exts] = RubyWasm::Packager::ALL_DEFAULT_EXTS
+        config[:default_exts] = config[:src][:all_default_exts]
         env_additional_exts = ENV["RUBY_WASM_ADDITIONAL_EXTS"] || ""
         unless env_additional_exts.empty?
           config[:default_exts] += "," + env_additional_exts
@@ -211,15 +211,18 @@ module RubyWasm
         "head" => {
           type: "github",
           repo: "ruby/ruby",
-          rev: "master"
+          rev: "master",
+          all_default_exts: RubyWasm::Packager::ALL_DEFAULT_EXTS,
         },
         "3.3" => {
           type: "tarball",
-          url: "https://cache.ruby-lang.org/pub/ruby/3.3/ruby-3.3.0.tar.gz"
+          url: "https://cache.ruby-lang.org/pub/ruby/3.3/ruby-3.3.0.tar.gz",
+          all_default_exts: "bigdecimal,cgi/escape,continuation,coverage,date,dbm,digest/bubblebabble,digest,digest/md5,digest/rmd160,digest/sha1,digest/sha2,etc,fcntl,fiber,gdbm,json,json/generator,json/parser,nkf,objspace,pathname,psych,racc/cparse,rbconfig/sizeof,ripper,stringio,strscan,monitor,zlib,openssl",
         },
         "3.2" => {
           type: "tarball",
-          url: "https://cache.ruby-lang.org/pub/ruby/3.2/ruby-3.2.3.tar.gz"
+          url: "https://cache.ruby-lang.org/pub/ruby/3.2/ruby-3.2.3.tar.gz",
+          all_default_exts: "bigdecimal,cgi/escape,continuation,coverage,date,dbm,digest/bubblebabble,digest,digest/md5,digest/rmd160,digest/sha1,digest/sha2,etc,fcntl,fiber,gdbm,json,json/generator,json/parser,nkf,objspace,pathname,psych,racc/cparse,rbconfig/sizeof,ripper,stringio,strscan,monitor,zlib,openssl",
         }
       }
 
