1576fd08672d7bc1d7a151c47a14cc8167b5e4f9
Yuta Saito <kateinoigakukun@gmail.com>
2024-09-04 10:54:17 +0000

    Use compiled API as fallback for rb-sys

--- ext/ruby_wasm/extconf.rb
+++ ext/ruby_wasm/extconf.rb
@@ -3,4 +3,7 @@
 require "mkmf"
 require "rb_sys/mkmf"
 
-create_rust_makefile("ruby_wasm/ruby_wasm")
+create_rust_makefile("ruby_wasm/ruby_wasm") do |r|
+  # We require head Ruby, so we need to fallback to compiled API
+  r.use_stable_api_compiled_fallback = true
+end
